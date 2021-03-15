a145de49a3c858b8edf6b16c37b9fb968c6a2173
llyu0966 <47392852+llyu0966@users.noreply.github.com>
2021-03-14 22:43:26 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo Task Descriptions:
-echo Task 1 - How many subway stations are for each of subway lines in New York City?
-echo I found a CSV file that contains all of subway stations in New York City from data.world. And my task here is to create a program that calculate the number of subway stations for each of city subway lines by using the dataset.
-echo Dataset: https://data.world/new-york-city/subway-stations
-echo Program: LineKVP.java
-echo Answer: Output.txt
+echo How many subway stations are for each of subway lines in New York City?
+echo Answer:
+javac LineKVP.java
 java LineKVP
